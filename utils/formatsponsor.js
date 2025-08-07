dfb92487bed423bc77b09bc6eef8fe974ae5c4c8
Jonathan-Harris1975 <johnbristol26@gmail.com>
2025-08-07 03:19:32 +0100

    Create formatsponsor.js

---
+++ utils/formatsponsor.js
@@ -0,0 +1,6 @@
+export function formatSponsor(book) {
+  if (!book?.title || !book?.author || !book?.link) {
+    return 'a brilliant AI book (details mysteriously missing)';
+  }
+  return `${book.title} by ${book.author} – ${book.link}`;
+}
