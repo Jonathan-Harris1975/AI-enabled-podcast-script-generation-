01edb296045b1143b6e044350be3cc4b1777e8c4
Jonathan-Harris1975 <johnbristol26@gmail.com>
2025-08-07 17:58:48 +0100

    Update getRandomSponsor.js

--- utils/getRandomSponsor.js
+++ utils/getRandomSponsor.js
@@ -1,7 +1,13 @@
-import books from '../data/books.json' assert { type: 'json' };
+import fs from 'fs';
+import path from 'path';
 
 export default function getRandomSponsor() {
+  const filePath = path.resolve('data/books.json');
+  const rawData = fs.readFileSync(filePath, 'utf-8');
+  const books = JSON.parse(rawData);
+
   const i = Math.floor(Math.random() * books.length);
   const { title, shortUrl } = books[i];
+
   return { title, shortUrl };
 }
