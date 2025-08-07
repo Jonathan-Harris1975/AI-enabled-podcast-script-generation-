c8f90a34398248a74dbcd53fbf7d415c5a0fef7c
Jonathan-Harris1975 <johnbristol26@gmail.com>
2025-08-07 03:25:25 +0100

    Create getTuringQuote.js

---
+++ utils/getTuringQuote.js
@@ -0,0 +1,13 @@
+const turingQuotes = [
+  "Sometimes it is the people no one can imagine anything of who do the things no one can imagine.",
+  "We can only see a short distance ahead, but we can see plenty there that needs to be done.",
+  "A computer would deserve to be called intelligent if it could deceive a human into believing that it was human.",
+  "Those who can imagine anything, can create the impossible.",
+  "Machines take me by surprise with great frequency.",
+  "I propose to consider the question, 'Can machines think?'"
+];
+
+export default function getTuringQuote() {
+  const randomIndex = Math.floor(Math.random() * turingQuotes.length);
+  return turingQuotes[randomIndex];
+}
