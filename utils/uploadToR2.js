49efd10964ac5444215e8116cf064dcb64c74e40
Jonathan-Harris1975 <johnbristol26@gmail.com>
2025-08-07 05:27:05 +0100

    Update and rename r2Client.js to uploadToR2.js

---
+++ utils/uploadToR2.js
@@ -0,0 +1,34 @@
+// utils/uploadToR2.js
+import { S3Client, PutObjectCommand } from '@aws-sdk/client-s3';
+
+const {
+  R2_ACCESS_KEY,
+  R2_SECRET_KEY,
+  R2_BUCKET,
+  R2_ENDPOINT
+} = process.env;
+
+const s3 = new S3Client({
+  region: 'auto',
+  endpoint: R2_ENDPOINT,
+  credentials: {
+    accessKeyId: R2_ACCESS_KEY,
+    secretAccessKey: R2_SECRET_KEY
+  }
+});
+
+/**
+ * Uploads a file to R2.
+ * @param {string} fileName - Name of the file to store (e.g. 'episode-001.txt')
+ * @param {string} content - Plain text content to upload
+ */
+export async function uploadToR2(fileName, content) {
+  const command = new PutObjectCommand({
+    Bucket: R2_BUCKET,
+    Key: fileName,
+    Body: content,
+    ContentType: 'text/plain'
+  });
+
+  await s3.send(command);
+}
