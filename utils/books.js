4a6dcc92ca658bb816590b45e6c70dd3a85e6db1
Jonathan-Harris1975 <johnbristol26@gmail.com>
2025-08-07 01:47:52 +0100

    Update books.js

--- utils/books.js
+++ utils/books.js
@@ -1,10 +1,9 @@
-// utils/books.js
-export default async function getRandomBook() {
-  const booksModule = await import('./books.json', {
-    assert: { type: 'json' }
-  });
+import fs from 'fs';
 
-  const books = booksModule.default;
+const rawData = fs.readFileSync(new URL('./books.json', import.meta.url));
+const books = JSON.parse(rawData);
+
+export default function getRandomBook() {
   const index = Math.floor(Math.random() * books.length);
   return books[index];
 }
