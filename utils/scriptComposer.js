762ac3800e7906df285850a75c865de90df6d413
Jonathan-Harris1975 <johnbristol26@gmail.com>
2025-08-07 15:37:23 +0100

    Update scriptComposer.js

--- utils/scriptComposer.js
+++ utils/scriptComposer.js
@@ -1,10 +1,11 @@
 import chunkText from './chunkText.js';
 import editAndFormat from './editAndFormat.js';
 
-export function composeScript(intro, chunks, outro) {
+function composeScript(intro, chunks, outro) {
   const fullScript = [intro, ...chunks, outro].join('\n\n');
   const cleaned = editAndFormat(fullScript);
-  const chunked = chunkText(cleaned); // returns array
-
-  return chunked.join('\n\n'); // final string
+  const chunked = chunkText(cleaned);
+  return chunked.join('\n\n');
 }
+
+export default composeScript;
