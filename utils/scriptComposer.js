dc1de2386a8239dde64dcf90344351bc6848753d
Jonathan-Harris1975 <johnbristol26@gmail.com>
2025-08-07 14:45:16 +0100

    Create scriptComposer.js

---
+++ utils/scriptComposer.js
@@ -0,0 +1,10 @@
+import chunkText from './chunkText.js';
+import editAndFormat from './editAndFormat.js';
+
+export function composeScript(intro, chunks, outro) {
+  const fullScript = [intro, ...chunks, outro].join('\n\n');
+  const cleaned = editAndFormat(fullScript);
+  const chunked = chunkText(cleaned); // returns array
+
+  return chunked.join('\n\n'); // final string
+}
