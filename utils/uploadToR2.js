bb2f8818d4dab82b03efbfe4f35e1d72639f039e
Jonathan-Harris1975 <johnbristol26@gmail.com>
2025-08-08 14:04:44 +0100

    Update uploadToR2.js

--- utils/uploadToR2.js
+++ utils/uploadToR2.js
@@ -1,11 +1,13 @@
 // utils/uploadToR2.js
+import fs from 'fs';
 import { S3Client, PutObjectCommand } from '@aws-sdk/client-s3';
 
 const {
   R2_ACCESS_KEY,
   R2_SECRET_KEY,
   R2_BUCKET,
-  R2_ENDPOINT
+  R2_ENDPOINT,
+  R2_PUBLIC_BASE_URL // e.g. https://your-bucket.r2.cloudflarestorage.com
 } = process.env;
 
 const s3 = new S3Client({
@@ -18,17 +20,24 @@ const s3 = new S3Client({
 });
 
 /**
- * Uploads a file to R2.
- * @param {string} fileName - Name of the file to store (e.g. 'episode-001.txt')
- * @param {string} content - Plain text content to upload
+ * Uploads a file from disk to R2 and returns the public URL.
+ * 
+ * @param {string} localFilePath - Full path to local file
+ * @param {string} remoteKey - Key to use in R2 (e.g. 'raw-text/123/chunk-1.txt')
+ * @returns {Promise<string>} - Public R2 URL of the uploaded file
  */
-export async function uploadToR2(fileName, content) {
+export default async function uploadToR2(localFilePath, remoteKey) {
+  const fileBuffer = fs.readFileSync(localFilePath);
+
   const command = new PutObjectCommand({
     Bucket: R2_BUCKET,
-    Key: fileName,
-    Body: content,
+    Key: remoteKey,
+    Body: fileBuffer,
     ContentType: 'text/plain'
   });
 
   await s3.send(command);
+
+  const publicUrl = `${R2_PUBLIC_BASE_URL}/${remoteKey}`;
+  return publicUrl;
 }
