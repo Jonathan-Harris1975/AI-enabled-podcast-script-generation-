ba91eecfcc455550c16b9e64a1be50e463bbd188
Jonathan-Harris1975 <johnbristol26@gmail.com>
2025-08-08 14:17:40 +0100

    Delete utils/sanitize.js

--- utils/sanitize.js
+++
@@ -1,9 +0,0 @@
-// utils/sanitize.js
-export function sanitizeText(text = '') {
-  return String(text)
-    .replace(/[\r\n\t]+/g, ' ')  // Remove all line breaks/tabs
-    .replace(/\s+/g, ' ')        // Collapse multiple spaces
-    .replace(/["“”]/g, '')       // Strip all types of quotation marks
-    .replace(/[\[\]{}]/g, '')    // Remove any brackets or braces
-    .trim();                     // Trim leading/trailing whitespace
-}
