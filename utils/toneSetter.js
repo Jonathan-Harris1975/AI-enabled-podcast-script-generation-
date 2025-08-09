502ec6857fb02a21a9eabdc52dad839b19fe1966
Jonathan-Harris1975 <johnbristol26@gmail.com>
2025-08-09 21:50:49 +0100

    Create toneSetter.js

---
+++ utils/toneSetter.js
@@ -0,0 +1,24 @@
+// utils/toneSetter.js
+
+const tones = [
+  'Sarcastic',
+  'Witty',
+  'Dry as hell',
+  'Skeptical',
+  'Optimistic',
+  'Formal',
+  'Casual',
+  'Playful',
+  'Serious',
+  'Bold',
+  'Cautious',
+  'Confident',
+  'Inspirational',
+  'Friendly',
+  'Humorous'
+];
+
+export function getRandomTone() {
+  const idx = Math.floor(Math.random() * tones.length);
+  return tones[idx];
+}
