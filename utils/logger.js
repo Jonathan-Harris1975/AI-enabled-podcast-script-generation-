296026a196976f9729ad2cafdcc0947fad3459d9
Jonathan-Harris1975 <johnbristol26@gmail.com>
2025-08-09 14:08:16 +0100

    Update logger.js

--- utils/logger.js
+++ utils/logger.js
@@ -1,20 +1,24 @@
 // utils/logger.js
 import winston from 'winston';
+import { format } from 'logform';
 
-const { createLogger, format, transports } = winston;
+const { createLogger, transports } = winston;
 const { combine, timestamp, printf, colorize } = format;
 
 const logFormat = printf(({ level, message, timestamp }) => {
   return `${timestamp} [${level}]: ${message}`;
 });
 
+const logger = createLogger({
+  level: process.env.NODE_ENV === 'production' ? 'info' : 'debug',
+  format: combine(
+    colorize(),
+    timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
+    logFormat
+  ),
+  transports: [new transports.Console()]
+});
+
 export function getLogger(moduleName) {
-  return createLogger({
-    level: process.env.NODE_ENV === 'production' ? 'info' : 'debug',
-    format: combine(
-      colorize(),
-      timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
-      logFormat
-    ),
-    defaultMeta: { module: moduleName },
-    transports: [new transports.Console
+  return logger.child({ module: moduleName });
+}
